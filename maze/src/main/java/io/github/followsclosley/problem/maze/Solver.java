b2f909ffa8b01f8e0a3547bd967b9cb9109a47c9
Lavern Wilson <mlavwilson@gmail.com>
2022-04-29 11:29:53 -0400

    Update Solver.java

--- maze/src/main/java/io/github/followsclosley/problem/maze/Solver.java
+++ maze/src/main/java/io/github/followsclosley/problem/maze/Solver.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public interface Solver {
     /**
-     * Solves the given maze returning the path out.
+     * Solves the given maze returning the path from start to finish.
      *
      * @param maze The maze to be solved
      * @return A List of Coordinates starting from the beginning of the maze.
